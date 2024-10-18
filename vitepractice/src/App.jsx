79e511aa9b2d764f069723816267495242f2b166
Manish <manishpothu003@gmail.com>
2024-10-18 11:53:50 +0530

    manish

--- vitepractice/src/App.jsx
+++ vitepractice/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          mani<code>src/App.jsx</code> and save to test HMR
+          manish<code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
